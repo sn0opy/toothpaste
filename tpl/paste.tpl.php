b73a0d44a47b325fc22300b83d3ccb551f9c2dc8
Sascha Ohms <sasch9r@gmail.com>
2011-07-04 11:19:13 +0200

    damn, didnt realized, that the code is THAT old...

--- tpl/paste.tpl.php
+++ tpl/paste.tpl.php
@@ -1,6 +1,6 @@
-<script type="text/javascript" src="{@BASE}media/js/shBrush{@lang}.js"></script>
+<script type="text/javascript" src="{{@BASE}}media/js/shBrush{{@lang}}.js"></script>
 <div class="codeField">
-    <pre class="brush: {strtolower(@lang)}">
-{@paste}
+    <pre class="brush: {{strtolower(@lang)}}">
+{{@paste}}
     </pre>
 </div>
